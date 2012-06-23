a5f7cb21d04efc5cea3b65d2813defadec4fd6c6
felix@familie-freiberger.net <felix@familie-freiberger.net@08a9a4e8-5b1d-2c5f-8841-573ebdb07868>
2012-06-23 10:42:47 +0000

    Reformatted SatSolverTestUpdateTool.java to make Ben happy.

--- src/SatSolverTestUpdateTool.java
+++ src/SatSolverTestUpdateTool.java
@@ -1,4 +1,5 @@
 package prog2.project3.tests;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,55 +20,60 @@ import junit.framework.TestCase;
 
 public class SatSolverTestUpdateTool extends TestCase {
 	private static final String projectID = "project3", version = "1.1";
-	
+
 	@Test
 	public void test_Update() {
-		SatSolverTestUpdateTool.doUpdateTest("SatSolverTestUpdateTool", version);
+		SatSolverTestUpdateTool
+				.doUpdateTest("SatSolverTestUpdateTool", version);
 	}
-	
+
 	@Test
 	public static final void test_GetNewTests() {
 		try {
-			URL url = new URL("https://prog2tests.googlecode.com/svn/distribution/" + projectID + ".txt");
+			URL url = new URL(
+					"https://prog2tests.googlecode.com/svn/distribution/"
+							+ projectID + ".txt");
 			InputStreamReader streamReader = new InputStreamReader(
 					url.openStream());
 			BufferedReader reader = new BufferedReader(streamReader);
 			String releasePath = "";
 			List<String> releaseInfo = new ArrayList<String>();
-			while(true) {
+			while (true) {
 				String in = reader.readLine();
-				if(in == null) {
+				if (in == null) {
 					break;
 				} else {
-					if(in.length() > 0) {
+					if (in.length() > 0) {
 						releaseInfo.add(in);
 					}
 				}
 			}
-			
+
 			reader.close();
 			streamReader.close();
-			
-			
+
 			boolean newTestsInstalled = false;
-			
-			for(String item : releaseInfo) {
+
+			for (String item : releaseInfo) {
 				if (item.contains("|")) {
 					releasePath = item.replace('|', File.separatorChar);
 				} else {
 					File file = new File(releasePath + item);
-					if(!file.exists()) {
-						System.out.println("A new test file is available and will be downloaded: " + item);
+					if (!file.exists()) {
+						System.out
+								.println("A new test file is available and will be downloaded: "
+										+ item);
 						file.mkdirs();
 						downloadFile(item, releasePath);
 						newTestsInstalled = true;
 					}
 				}
 			}
-			
-			if(newTestsInstalled) {
-				System.out.println("New test files were installed. Please, refresh and run the tests again.\n" +
-						"To do this, select the Test-package in Package Explorer and press F5.");
+
+			if (newTestsInstalled) {
+				System.out
+						.println("New test files were installed. Please, refresh and run the tests again.\n"
+								+ "To do this, select the Test-package in Package Explorer and press F5.");
 				fail("New test files were installed. Please, refresh and run the tests again.");
 			}
 		} catch (Exception e) {
@@ -75,7 +81,7 @@ public class SatSolverTestUpdateTool extends TestCase {
 					+ "That happened: " + e.getMessage());
 		}
 	}
-	
+
 	public static final void doUpdateTest(final String testID,
 			final String currentVersion) {
 		try {
@@ -94,14 +100,16 @@ public class SatSolverTestUpdateTool extends TestCase {
 			}
 
 			if (!isUpToDate(currentVersion, remoteVersion)) {
-				System.out.println("You test is out of date.\n" + "You have version "
-						+ currentVersion + ", but the current version is " + remoteVersion + "\n" +
-								"You will be auto-updated to that version...");
-				
+				System.out.println("You test is out of date.\n"
+						+ "You have version " + currentVersion
+						+ ", but the current version is " + remoteVersion
+						+ "\n" + "You will be auto-updated to that version...");
+
 				try {
 					downloadTest(testID, updatePath);
-					System.out.println("Update completed. Refresh in Eclipse and rerun.\n" +
-							"To do this, select the Test-package in Package Explorer and press F5.");
+					System.out
+							.println("Update completed. Refresh in Eclipse and rerun.\n"
+									+ "To do this, select the Test-package in Package Explorer and press F5.");
 					fail("Update completed. Refresh in Eclipse and rerun.");
 				} catch (Exception e) {
 					System.out.println("ERROR: " + e.getMessage());
@@ -117,63 +125,66 @@ public class SatSolverTestUpdateTool extends TestCase {
 	}
 
 	public static final void openUrl(String url) {
-    	try {
-    	      if(java.awt.Desktop.isDesktopSupported() ) {
-    	            java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
-    	     
-    	            if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
-    	              java.net.URI uri = new java.net.URI(url);
-    	                  desktop.browse(uri);
-    	            }
-    	          }
-    	} catch (Exception e) {
-    		System.out.println("We tried to send you to a helpful page, but something went wrong.\n" +
-    				"Try going there manually: " + url);
-    	}
-    }
-	
+		try {
+			if (java.awt.Desktop.isDesktopSupported()) {
+				java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+
+				if (desktop.isSupported(java.awt.Desktop.Action.BROWSE)) {
+					java.net.URI uri = new java.net.URI(url);
+					desktop.browse(uri);
+				}
+			}
+		} catch (Exception e) {
+			System.out
+					.println("We tried to send you to a helpful page, but something went wrong.\n"
+							+ "Try going there manually: " + url);
+		}
+	}
+
 	private static final boolean isUpToDate(String currentS, String remoteS) {
 		int[] current = toVersion(currentS);
 		int[] remote = toVersion(remoteS);
-		
-		for(int i = 0; i < Math.min(current.length, remote.length); i++) {
+
+		for (int i = 0; i < Math.min(current.length, remote.length); i++) {
 			if (current[i] < remote[i])
 				return false;
 			else if (current[i] > remote[i])
 				return true;
 		}
-		
-		if(current.length < remote.length)
-			return false; //1.0.0 should be more up-to-date than 1.0
+
+		if (current.length < remote.length)
+			return false; // 1.0.0 should be more up-to-date than 1.0
 		else
 			return true;
 	}
-	
+
 	private static final int[] toVersion(String versionString) {
 		String[] split = versionString.split("\\.");
 		int[] version = new int[split.length];
-		
-		for(int i = 0; i < version.length; i++) {
+
+		for (int i = 0; i < version.length; i++) {
 			version[i] = Integer.valueOf(split[i]);
 		}
-		
+
 		return version;
 	}
-	
-	private static final void downloadFile(String fileName, String updatePath) throws Exception {
-	    updatePath = updatePath.replace('|', File.separatorChar);
 
-	    URL url = new URL("https://prog2tests.googlecode.com/svn/src/"
+	private static final void downloadFile(String fileName, String updatePath)
+			throws Exception {
+		updatePath = updatePath.replace('|', File.separatorChar);
+
+		URL url = new URL("https://prog2tests.googlecode.com/svn/src/"
 				+ fileName);
-	    ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-	    FileOutputStream fos = new FileOutputStream(updatePath + fileName);
-	    fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-	    
-	    fos.close();
-	    rbc.close();
+		ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+		FileOutputStream fos = new FileOutputStream(updatePath + fileName);
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+
+		fos.close();
+		rbc.close();
 	}
-	
-	private static final void downloadTest(String testName, String updatePath) throws Exception {
+
+	private static final void downloadTest(String testName, String updatePath)
+			throws Exception {
 		downloadFile(testName + ".java", updatePath);
 	}
 }
