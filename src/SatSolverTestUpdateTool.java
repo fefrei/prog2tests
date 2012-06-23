9253f5fb74578bdbaa7eea94b4e9918e640557c9
felix@familie-freiberger.net <felix@familie-freiberger.net@08a9a4e8-5b1d-2c5f-8841-573ebdb07868>
2012-06-23 10:24:39 +0000

    Updated SVN for project 3.

--- src/SatSolverTestUpdateTool.java
+++ src/SatSolverTestUpdateTool.java
@@ -1,4 +1,4 @@
-package prog2.project2.tests;
+package prog2.project3.tests;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,13 +17,13 @@ import junit.framework.TestCase;
 // Felix Freiberger, 2012
 // ONLY FOR USE IN PROJECT 2
 
-public class SeamCarverTestUpdateTool extends TestCase {
-	static final String projectID = "project2";
-	static final String version = "1.1";
+public class SatSolverTestUpdateTool extends TestCase {
+	static final String projectID = "project3";
+	static final String version = "1.0";
 	
 	@Test
 	public void test_Update() {
-		SeamCarverTestUpdateTool.doUpdateTest("UpdateTool", version);
+		SatSolverTestUpdateTool.doUpdateTest("SatSolverTestUpdateTool", version);
 	}
 	
 	@Test
@@ -33,15 +33,15 @@ public class SeamCarverTestUpdateTool extends TestCase {
 			InputStreamReader streamReader = new InputStreamReader(
 					url.openStream());
 			BufferedReader reader = new BufferedReader(streamReader);
-			String releasePath = reader.readLine();
-			List<String> releases = new ArrayList<String>();
+			String releasePath = "";
+			List<String> releaseInfo = new ArrayList<String>();
 			while(true) {
 				String in = reader.readLine();
 				if(in == null) {
 					break;
 				} else {
 					if(in.length() > 0) {
-						releases.add(in);
+						releaseInfo.add(in);
 					}
 				}
 			}
@@ -49,21 +49,27 @@ public class SeamCarverTestUpdateTool extends TestCase {
 			reader.close();
 			streamReader.close();
 			
-			releasePath = releasePath.replace('|', File.separatorChar);
 			
 			boolean newTestsInstalled = false;
 			
-			for(String item : releases) {
-				File file = new File(releasePath + item);
-				if(!file.exists()) {
-					System.out.println("A new test file is available and will be downloaded: " + item);
-					downloadFile(item, releasePath);
-					newTestsInstalled = true;
+			for(String item : releaseInfo) {
+				if (item.contains("|")) {
+					releasePath = item.replace('|', File.separatorChar);
+				} else {
+					File file = new File(releasePath + item);
+					if(!file.exists()) {
+						System.out.println("A new test file is available and will be downloaded: " + item);
+						file.mkdirs();
+						downloadFile(item, releasePath);
+						newTestsInstalled = true;
+					}
 				}
 			}
 			
 			if(newTestsInstalled) {
-				System.out.println("New test files were installed. Please, refresh (F5) and run the tests again.");
+				System.out.println("New test files were installed. Please, refresh and run the tests again.\n" +
+						"To do this, select the Test-package in Package Explorer and press F5.");
+				fail("New test files were installed. Please, refresh and run the tests again.");
 			}
 		} catch (Exception e) {
 			fail("Failed to check for new tests because an error occurred. Maybe you are offline?\n"
@@ -95,8 +101,9 @@ public class SeamCarverTestUpdateTool extends TestCase {
 				
 				try {
 					downloadTest(testID, updatePath);
-					System.out.println("Update completed. Refresh (F5) in Eclipse and rerun.");
-					fail("Update completed. Refresh (F5) in Eclipse and rerun.");
+					System.out.println("Update completed. Refresh in Eclipse and rerun.\n" +
+							"To do this, select the Test-package in Package Explorer and press F5.");
+					fail("Update completed. Refresh in Eclipse and rerun.");
 				} catch (Exception e) {
 					System.out.println("ERROR: " + e.getMessage());
 					fail("Your version is out-of-date, but the update failed.");
