06af35a72e8bcfc1e6b326e535bdeedecece840a
felix@familie-freiberger.net <fefrei@users.noreply.github.com>
2012-06-24 16:07:49 +0000

    updated TseitinFelixTest to 1.3 updated TestUtilFelix to 1.1.1

--- src/TseitinFelixTest.java
+++ src/TseitinFelixTest.java
@@ -12,7 +12,7 @@ import prog2.project3.propositional.FormulaReader;
 import prog2.project3.propositional.PropositionalFormula;
 
 public class TseitinFelixTest {
-	static final String VERSION = "1.2";
+	static final String VERSION = "1.3";
 
 	// @Test
 	// public void testGenerateTestData() {
@@ -52,14 +52,13 @@ public class TseitinFelixTest {
 		List<String> testData = TestUtilFelix
 				.parseDataFile("examples|Felix|TseitinFelixTestData1.txt");
 		if (testData.size() != 94 * 3)
-			fail("TseitinFelixTestData1.txt has wrong size or could not be read.\n" +
-					"You can simply delete it to fetch a new copy.\n"
+			fail("TseitinFelixTestData1.txt has wrong size or could not be read.\n"
+					+ "You can simply delete it to fetch a new copy.\n"
 					+ "If you cannot solve this problem, file a support ticket.");
 
-		List<Integer> failedTests = new LinkedList<Integer>();
-		List<String> failureMessages = new LinkedList<String>();
+		List<String> results = new LinkedList<String>();
 
-		for (int testID = 1; testID <= 81; testID++) {
+		for (int testID = 1; testID <= 94; testID++) {
 			String formulaString = testData.get((testID - 1) * 3);
 			String expectedFormulaOutString = testData
 					.get((testID - 1) * 3 + 1);
@@ -73,8 +72,7 @@ public class TseitinFelixTest {
 			String cnfString = TestUtilFelix.cnfToString(cnf);
 
 			if (!expectedFormulaOutString.equals(formulaOutString)) {
-				failedTests.add(testID);
-				failureMessages
+				results
 						.add("Test "
 								+ testID
 								+ " called FormulaReader.readFormulaFromString with this argument:\n"
@@ -87,8 +85,7 @@ public class TseitinFelixTest {
 				continue;
 			}
 			if (!expectedCnfString.equals(cnfString)) {
-				failedTests.add(testID);
-				failureMessages
+				results
 						.add("Test "
 								+ testID
 								+ " called FormulaReader.readFormulaFromString with this argument:\n"
@@ -102,13 +99,11 @@ public class TseitinFelixTest {
 								+ "\nPlease, check PropositionalFormula.getConjunctiveNormalForm for errors.");
 				continue;
 			}
+			results.add(null);
 		}
 
-		if (failedTests.size() > 0) {
-			TestUtilFelix.failAndExplain(
-					"TseitinFelixTest#testVerifyTseitinData1", 94,
-					failedTests.toArray(new Integer[0]),
-					failureMessages.toArray(new String[0]));
-		}
+		TestUtilFelix.checkFailAndExplain(
+				"TseitinFelixTest#testVerifyTseitinData1",
+				results.toArray(new String[0]));
 	}
 }
