2ca7f198eb5dc17a02c54c06d15dea1d4a421295
felix@familie-freiberger.net <fefrei@users.noreply.github.com>
2012-07-27 20:45:59 +0000

    added EvilFelixTest.java

---
+++ src/EvilFelixTest.java
@@ -0,0 +1,38 @@
+package prog2.project4.tests.prog2tests;
+
+import org.junit.Test;
+
+import prog2.project4.tests.TestBase;
+
+public class EvilFelixTest extends TestBase {
+
+	public final String VERSION = "1.0";
+	
+	@Test
+	public void test_Update(){
+		CompilerTestUpdateTool.doUpdateTest("EvilFelixTest", VERSION);
+	}
+
+	@Test
+	public void testReallyBigExpression() throws Exception {
+		final int depth = 15; 
+
+		String badExp = "x";
+		
+		for (int i = 0; i < depth; i++) {
+			badExp = "(" + badExp + " * " + badExp + ")";
+		}
+
+		try {
+			assertResult("int evil(int x) { return " + badExp + "; }", 1, 1);
+		} catch (Exception e) {
+			System.out.println("Your Compiler threw an exception while compiling my program.\n" +
+					"This *may* be because ist has a *really* big expression.\n" +
+					"Since there is a surrounding function, there is *no* guarantee that 12 registers are enough.\n" +
+					"This is not a severe problem, because it is really mean*, but you should still consider fixing it.\n" +
+					"* Tobias hinted that they will not test this.\n");
+			throw new Exception("Your compile threw an exception. See console.", e);
+		}
+	}
+
+}
