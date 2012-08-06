e10602ff0d7857051b6ff13619709f8accdb4161
felix@familie-freiberger.net <felix@familie-freiberger.net@08a9a4e8-5b1d-2c5f-8841-573ebdb07868>
2012-08-06 19:15:49 +0000

    updated CompilerFelixTest to 1.3 Fixes issue 69.

--- src/CompilerFelixTest.java
+++ src/CompilerFelixTest.java
@@ -1,17 +1,19 @@
 package prog2.project4.tests.prog2tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import prog2.project4.driver.Configuration;
+import prog2.project4.driver.ParseEntity;
+import prog2.project4.parser.ParserException;
 import prog2.project4.tests.TestBase;
 import prog2.project4.tree.Tree;
 import prog2.project4.tree.TreeFactory;
 
 public class CompilerFelixTest extends TestBase {
 
-	public final String VERSION = "1.2.1";
+	public final String VERSION = "1.3";
 
 	@Test
 	public void test_Update() {
@@ -40,8 +42,39 @@ public class CompilerFelixTest extends TestBase {
 		Tree listTree = factory.makeList();
 		Tree innerTree = factory.makeList();
 		listTree.setOperand(50, innerTree);
+
 		assertTrue(listTree.getOperand(50) == innerTree);
-		assertTrue(listTree.getOperand(42) == null); // was never set, must be null
+
+		// was never set, must be null
+		assertTrue(listTree.getOperand(42) == null);
 	}
 
+	/**
+	 * Tests some edge cases for scopes
+	 */
+	@Test
+	public void scopeIncompleteDeclarationTest() {
+		final String prog = "int f(int a) { if (a == 0) int b = 5; else int c = 5; c = c + 5; return c; }";
+
+		assertResult(prog, 10, 42);
+		assertResult(prog, 10, -42);
+	}
+
+	/**
+	 * Tests that you don't allow double declaration
+	 * 
+	 * @throws ParserException
+	 */
+	@Test
+	public void scopeDoubleDeclarationTest() throws ParserException {
+		assertNameException(ParseEntity.PRG, "int f(int x) { int a = 0; int a = 1; return a; };");
+	}
+
+	/**
+	 * Tests that you handle multiple scopes
+	 */
+	@Test
+	public void scopeNestedDeclaration() {
+		assertResult("int f(int x) { int a = 5; { int a = 10; } return a; }", 5, 42);
+	}
 }
